0c5f523ee367c10952f2e4fe28269189892b474c
Ferglow <fernando.a.h@outlook.com>
2025-06-30 19:48:30 -0600

    fichero my-first-io

--- baby-steps.js
+++ baby-steps.js
@@ -1,9 +1,5 @@
-'use strict'
-
-let result = 0
-
-for (let i = 2; i < process.argv.length; i++) {
-  result += Number(process.argv[i])
+let result = 0;
+for (i = 2; i < process.argv.length; i++) {
+    result += Number(process.argv[i]);
 }
-
-console.log(result)
+console.log(result);
---
+++ my-first-io.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+const contents = fs.readFileSync(process.argv[2], 'utf8');
+const lines = contents.split('\n').length - 1;
+
+console.log(lines);
